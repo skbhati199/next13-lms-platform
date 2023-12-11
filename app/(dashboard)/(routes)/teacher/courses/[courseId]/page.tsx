b127c1262ccef882416316f3e928b9a6928415a1
Sonu Kumar <skbhati199@gmail.com>
2023-12-11 19:19:53 +0530

    update

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -6,14 +6,14 @@ import { db } from "@/lib/db";
 import { IconBadge } from "@/components/icon-badge";
 import { Banner } from "@/components/banner";
 
-import { TitleForm } from "./_components/title-form";
-import { DescriptionForm } from "./_components/description-form";
-import { ImageForm } from "./_components/image-form";
-import { CategoryForm } from "./_components/category-form";
-import { PriceForm } from "./_components/price-form";
-import { AttachmentForm } from "./_components/attachment-form";
-import { ChaptersForm } from "./_components/chapters-form";
-import { Actions } from "./_components/actions";
+import { TitleForm } from "../../_components/title-form";
+import { DescriptionForm } from "../../_components/description-form";
+import { ImageForm } from "../../_components/image-form";
+import { CategoryForm } from "../../_components/category-form";
+import { PriceForm } from "../../_components/price-form";
+import { AttachmentForm } from "../../_components/attachment-form";
+import { ChaptersForm } from "../../_components/chapters-form";
+import { Actions } from "../../_components/actions";
 
 const CourseIdPage = async ({
   params
